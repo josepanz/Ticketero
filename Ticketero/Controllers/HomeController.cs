3c9a5cffc226b1d161545e9c5098abf9d9298dcc
José Panza <jpanza@uaa.edu.py>
2020-11-29 14:41:56 -0300

    Commit Inicial del Sistema de Tickets

---
+++ Ticketero/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ticketero.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Sistema de Gestion de Ticket simple, para Gestion de Turnos Diarios por Caja.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contactenos.";
+
+            return View();
+        }
+    }
+}
